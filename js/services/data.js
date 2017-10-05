70b91831504184a55836042b34fd272b0c901348
Dan <dan.mckenna@digitalskillsacademy.me>
2017-10-05 18:12:40 +0100

    added local storage

--- js/services/data.js
+++ js/services/data.js
@@ -2,15 +2,28 @@
 
 angular.module("toDoList")
 .service("dataService", function($http){
-  
+
   this.getTodos = function(callback){
-          $http.get('data/todo.json')
+          localStorage.getItem("task")
                 .then(callback)
               };
 this.deleteToDo = function(toDo){
   console.log("the " + toDo.name +  " to do item has been deleted")
 };
   this.saveToDos = function(toDo){
+    localStorage.setItem("task", toDo.name);
     console.log("the " + toDo.name +  " to do item has been saved")
   };
 });
+// Code reuse https://www.tutorialrepublic.com/html-tutorial/html5-web-storage.php
+
+/* Check if the localStorage object exists
+if(localStorage){
+    // Store data
+    localStorage.setItem("first_name", "Peter");
+
+    // Retrieve data
+    alert("Hi, " + localStorage.getItem("first_name"));
+} else{
+    alert("Sorry, your browser do not support local storage.");
+}*/
