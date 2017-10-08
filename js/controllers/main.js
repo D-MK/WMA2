843c82e9b0737ea49cefb8c90de5c4e31f6be358
Dan <dan.mckenna@digitalskillsacademy.me>
2017-10-08 21:12:02 +0100

    version 1.0 upload

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -3,34 +3,52 @@
 angular.module("toDoList")
 .controller("myMainCtrl", function($scope, dataService){
   //adding new toDo
-
   $scope.addToDo = function(){
+$scope.toDos = $scope.toDos || [];
+/*ensuring if no array is present that it gets created
+Code reuse ref: https://stackoverflow.com/questions/26273043/cannot-read-property-push-of-null
+*/
     var toDo = {name: $scope.newToDo };
-    $scope.toDos.unshift(toDo);
-    $scope.newToDo=''/*
+  $scope.toDos.push(toDo);
+//push newly created toDo into the array
+    $scope.newToDo=""/*
     Code Reuse Ref: http://www.ustutorials.com/angularjs-tutorials/create-angularjs-to-do-application.php
     cleares the input once enter any text in input
     */
+    localStorage.setItem("name", JSON.stringify($scope.toDos));
+    //add the array as a string to local storage
   };
   //calling saved toDos
   dataService.getTodos(function(response){
     console.log(response.data);
-    $scope.toDos = response.data;
+//calling the list from local storage
+    $scope.toDos = JSON.parse(localStorage.getItem("name"));
+
   });
 //deleting toDo
   $scope.deleteToDo = function(toDo, $index){
     dataService.deleteToDo(toDo);
     $scope.toDos.splice($scope.toDos.indexOf(toDo), 1);
-    /*Code Reuse to ensure correct item is deleted
+    localStorage.setItem("name", JSON.stringify($scope.toDos));
+    /* Deleting the toDo from the array,
+    and also updating the local storage to reflect the change
+    Code Reuse to ensure correct item is deleted
     Ref: https://stackoverflow.com/questions/16118762/angularjs-wrong-index-after-orderby
     */
   };
 //saving
   $scope.saveToDos = function(){
+    console.log("saving");
+localStorage.setItem("name", JSON.stringify($scope.toDos));
+//var saveToDo = {name: "Save"}
+//$scope.toDos.push(saveToDo);
+//$scope.toDos.splice($scope.toDos.indexOf(saveToDo), 1);
+
   var filteredToDos =  $scope.toDos.filter(function(todo){
-      if(to.edited){
-        return todo;
-      }
+  //    if(to.edited){
+    //    return todo;
+
+      //}
 
     })
     dataService.saveToDos(filteredToDos);
--- js/services/data.js
+++ js/services/data.js
@@ -1,29 +1,20 @@
 "use strict";
 
+
+
+
 angular.module("toDoList")
 .service("dataService", function($http){
 
-  this.getTodos = function(callback){
+  this.getTodos =  function(callback){
           $http.get('data/todo.json')
-                .then(callback)
+                .then(callback);
               };
 this.deleteToDo = function(toDo){
-  console.log("the " + toDo.name +  " to do item has been deleted")
+  console.log("The to do item has been deleted");
 };
   this.saveToDos = function(toDo){
-    localStorage.setItem("task", toDo.name);
-    console.log("the " + toDo.name +  " to do item has been saved")
+
+    console.log("The to do item has been saved");
   };
 });
-
-
-// Check if the localStorage object exists
-if(localStorage){
-    // Store data
-    localStorage.setItem("task", toDo.name);
-
-    // Retrieve data
-    alert("Hi, " + localStorage.getItem("task"));
-} else{
-    alert("Sorry, your browser do not support local storage.");
-}
